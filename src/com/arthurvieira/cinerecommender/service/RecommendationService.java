7f7d7b9f536a96bed03a078ba9726b25e8ca60dd
arthurvieirasilvaa <arthursilvavieira03@gmail.com>
2026-03-19 11:21:12 -0300

    Iniciei a classe RecommendationService

--- src/com/arthurvieira/cinerecommender/service/RecommendationService.java
+++ src/com/arthurvieira/cinerecommender/service/RecommendationService.java
@@ -1,4 +1,14 @@
 package com.arthurvieira.cinerecommender.service;
 
+import com.arthurvieira.cinerecommender.repository.ContentRepository;
+import com.arthurvieira.cinerecommender.repository.UserRepository;
+
 public class RecommendationService {
+    private final ContentRepository contentRepository;
+    private final UserRepository userRepository;
+
+    public RecommendationService(ContentRepository contentRepository, UserRepository userRepository) {
+        this.contentRepository = contentRepository;
+        this.userRepository = userRepository;
+    }
 }
